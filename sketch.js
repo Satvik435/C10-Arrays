3f0b58749bbe854a6c15f6dce2858a8595e18267
Satvik435 <86778274+Satvik435@users.noreply.github.com>
2021-07-05 16:31:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var marks = [12,34,54,69]
+
+var sum = marks[0]+marks[1]+marks[2]+marks[3]
+var avg = sum / marks.length 
+function setup() 
+{console.log (avg)
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
